865a757bbaf2a05e56192d8a67f372a8c2921f19
osadolortech <91027519+osadolortech@users.noreply.github.com>
2022-08-14 16:06:34 +0100

    Update settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -69,6 +69,7 @@ AUTH_USER_MODEL = 'user.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
