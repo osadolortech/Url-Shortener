2a03c9fbd330e769da6ecfad7749be8e7490899d
osadolortech <91027519+osadolortech@users.noreply.github.com>
2022-08-14 16:33:41 +0100

    Update settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -162,13 +162,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["*"]
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
