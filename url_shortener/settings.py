91957359267ecb4c20324f57f67422a5f0e365e8
osadolortech <91027519+osadolortech@users.noreply.github.com>
2022-08-16 10:20:43 +0100

    Update settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 import dj_database_url
 from datetime import timedelta
 import django_heroku
